1060639f74c32921148b1d146c2e8c555baadfe7
Laura Pareja-Prieto <lpareja@drury.edu>
2022-10-30 19:38:18 +0100

    update

--- BestTimeBuySellStock/test_pytest.py
+++ BestTimeBuySellStock/test_pytest.py
@@ -1,8 +1,21 @@
 from bestTimeBuySellStockII import *
 
-nums = [1,2,3,4,5,6]
+s1 = [1,2,3,4,5,6]
+s2 = [7,1,5,3,6,4]
+s3 = [1,2,3,4,5]
+s4 = [7,6,4,3,1]
+
 
 test = Solution()
 
-def test_Func():
-    assert test.maxProfit(nums) == 5
+def test_s1():
+    assert test.maxProfit(s1) == 5
+
+def test_s2():
+    assert test.maxProfit(s2) == 7
+
+def test_s3():
+    assert test.maxProfit(s3) == 4
+
+def test_s4():
+    assert test.maxProfit(s4) == 0
